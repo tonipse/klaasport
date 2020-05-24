d89a8ac86ea6245a01d2a99011316891dd0f3c61
Toni Peise <toni.peise@gmx.net>
2020-05-24 18:36:15 +0200

    1.0: init old files to parcel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 //Import .js here import "./js/";
+import fullpage from "fullpage.js";
 
+new fullpage("#fullpage", {
+   navigation: true,
+   sectionsColor: ["#ff5f45", "#0798ec", "#fc6c7c", "grey"],
+});
 console.log("index.js is working");
